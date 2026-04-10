3e7d70f783f506c5d70d08106900788a337a10d6
Magesh Priya <58501455+Mageshpriya2020@users.noreply.github.com>
2026-04-10 08:07:26 +0530

    initial commit

---
+++ graalvm-native/src/main/java/com/graalvm/project/GraalvmNativeApplication.java
@@ -0,0 +1,13 @@
+package com.graalvm.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmNativeApplication.class, args);
+	}
+
+}
---
+++ graalvm-native/src/test/java/com/graalvm/project/GraalvmNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graalvm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
